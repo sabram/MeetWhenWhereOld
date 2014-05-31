557555be4bd5febfec5c1ccabc564ff0898c20e4
sabram <shaunabram@gmail.com>
2014-05-31 09:49:36 -0700

    moved the MeetingCtrl controller into an angular module: letsMeetModule

--- src/main/webapp/meet.js
+++ src/main/webapp/meet.js
@@ -1,6 +1,8 @@
-function MeetingCtrl($scope, $http) {
+var letsMeetModule = angular.module('letsMeet', []);
+
+letsMeetModule.controller('MeetingCtrl', function($scope, $http) {
     $http.get('http://localhost:8080/meeting').
         success(function(data) {
             $scope.meeting = data;
         });
-}
+});

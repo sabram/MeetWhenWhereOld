b0cc55e256028e669abcf938478b21e5ea9472fd
sabram <shaunabram@gmail.com>
2014-05-31 09:26:48 -0700

    Angular controller renamed from Hello to MeetingCtrl

--- src/main/webapp/meet.js
+++ src/main/webapp/meet.js
@@ -1,4 +1,4 @@
-function Hello($scope, $http) {
+function MeetingCtrl($scope, $http) {
     $http.get('http://localhost:8080/meeting').
         success(function(data) {
             $scope.meeting = data;

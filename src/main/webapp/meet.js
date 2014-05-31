ce87e4e33a0043a31be2f16222dbe201df1bd86f
sabram <shaunabram@gmail.com>
2014-05-31 16:23:36 -0700

    updated to use Rectangular for REST calls

--- src/main/webapp/meet.js
+++ src/main/webapp/meet.js
@@ -1,8 +1,15 @@
-var letsMeetModule = angular.module('letsMeet', []);
+var letsMeetModule = angular.module('letsMeet', ['restangular']);
 
-letsMeetModule.controller('MeetingCtrl', function($scope, $http) {
-    $http.get('http://localhost:8080/meeting').
-        success(function(data) {
-            $scope.meeting = data;
-        });
+letsMeetModule.controller('MeetingCtrl', function($scope, $http, Restangular) {
+
+//    $http.get('http://localhost:8080/meeting').
+//        success(function(data) {
+//            $scope.meeting = data;
+//        });
+
+//    $scope.meeting = Restangular.oneUrl('googlers', 'http://localhost:8080/meeting').get();
+//    $scope.meeting = Restangular.one('http://localhost:8080/meeting');
+//    $scope.meeting = Restangular.all('http://localhost:8080/meeting');
+    $scope.meeting = Restangular.one('meeting').get().$object;
+
 });

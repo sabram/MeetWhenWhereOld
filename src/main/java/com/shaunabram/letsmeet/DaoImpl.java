c37ab9d731a19db74f54f3c5e3454ac0ee788ca6
sabram <shaunabram@gmail.com>
2014-06-21 09:32:01 -0700

    added basic logback xml config for logging

--- src/main/java/com/shaunabram/letsmeet/DaoImpl.java
+++ src/main/java/com/shaunabram/letsmeet/DaoImpl.java
@@ -2,13 +2,20 @@ package com.shaunabram.letsmeet;
 
 import com.google.common.collect.Lists;
 import org.springframework.stereotype.Repository;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.List;
 
 @Repository
 public class DaoImpl implements Dao {
 
-    @Override
+	private static final Logger logger = LoggerFactory.getLogger(DaoImpl.class);
+
+	public DaoImpl() {
+		logger.debug("In DaoImpl constructor");
+	}
+
+	@Override
     public String getStuff() {
         return "stuff";
     }

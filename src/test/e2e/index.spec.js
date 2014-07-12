c33f4478bf7345c8094236d99c2cc2187ae6dd8c
sabram <shaunabram@gmail.com>
2014-07-12 15:13:01 -0700

    can now do: gradle guitest

--- src/test/e2e/index.spec.js
+++ src/test/e2e/index.spec.js
@@ -1,6 +1,6 @@
 describe('letsmeet homepage', function() {
     it('should display correct title', function() {
-        browser.get('http://localhost:8080');
+        browser.get('http://localhost:9090/LetsMeet');
         expect(browser.getTitle()).toBe("Let's Meet!");
 //        expect('hello').toEqual('hello');
     });

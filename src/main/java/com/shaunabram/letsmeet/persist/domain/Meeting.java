3d0e8561aa0e5af00b5e4529529f5297c9014b95
sabram <shaunabram@gmail.com>
2014-07-15 07:39:20 -0700

    added MeetingMappingIntegrationTests

--- src/main/java/com/shaunabram/letsmeet/persist/domain/Meeting.java
+++ src/main/java/com/shaunabram/letsmeet/persist/domain/Meeting.java
@@ -1,4 +1,15 @@
 package com.shaunabram.letsmeet.persist.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "MEETING")
 public class Meeting {
+
+	@Id
+	@Column(name = "MEETING_ID")
+	long id;
+
+	String name;
 }

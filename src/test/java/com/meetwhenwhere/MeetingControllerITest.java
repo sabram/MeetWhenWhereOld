3bb248362814edd68ee21b6902a38f28f73128c2
sabram <shaunabram@gmail.com>
2014-08-11 07:19:37 -0700

    Deleted JPA classes involving EntityManager and JPAAssertions. Going to keep things simple and use spring boot defaults for now.

--- src/test/java/com/meetwhenwhere/MeetingControllerITest.java
+++ src/test/java/com/meetwhenwhere/MeetingControllerITest.java
@@ -1,26 +1,16 @@
 package com.meetwhenwhere;
 
-import com.meetwhenwhere.config.JPAConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
 import java.util.List;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {JPAConfiguration.class})
 public class MeetingControllerITest {
-
-	@Autowired
-	@Qualifier(value = "entityManager")
-	EntityManager manager;
 
 	@Inject
 	@SuppressWarnings("SpringJavaAutowiringInspection") //IntelliJ doesn't seem to parse out annotations too well. All runs OK.
